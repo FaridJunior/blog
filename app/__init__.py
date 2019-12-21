184b1395490e1e20258e5cc0b0d1d707e1a6eb7a
mohamedfaridelsaid <mohammed_eliraqi@yahoo.com>
2019-12-21 15:40:33 +0200

    add models and make migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from config import Config
-from flask_bootstrap import Bootstrap
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate( app , db )
 
-from app import routes
+
+from app import routes, models
+from app.models import User, Post
