1ddc30d7eb2f6fd477f17df492128948e5d60606
mohamedfaridelsaid <mohammed_eliraqi@yahoo.com>
2020-01-05 17:22:21 +0200

    add bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
-
+from flask_bootstrap import Bootstrap
 
 import logging
 from logging.handlers import SMTPHandler
@@ -20,6 +20,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail =Mail(app)
+Bootstrap(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
